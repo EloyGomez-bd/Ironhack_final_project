57d5e2ae02d5beac884d8d934fb0ebd2a0c1a390
EloyGomez <emoreno@outlook.dk>
2021-04-20 23:22:27 +0200

    pipeline preliminary adjustments

--- p_wrangling/m_wrangling.py
+++ p_wrangling/m_wrangling.py
@@ -2,57 +2,60 @@
 columns_to_fill_zeros = ['FATAL', 'INJURE', 'INC_PRES']
 
 columns_to_fill_empty = ['ONSHORE_CITY_NAME', 'OFF_ACCIDENT_ORIGIN', 'ONSHORE_COUNTY_NAME',
-                         'ONSHORE_STATE_ABBREVIATION', 'OFFSHORE_STATE_ABBREVIATION']
+                         'OFFSHORE_COUNTY_NAME', 'ONSHORE_STATE_ABBREVIATION', 'OFFSHORE_STATE_ABBREVIATION']
 
-def on_offshore(df_list, column_list, list_index):
+def on_offshore(df_list, index):
 
     """ Fix locations of datesets hl2010toPresent and gtggungs2010toPresent """
 
     print('Classifying onshore and offshore incidents...')
 
-    for i in list_index:
-
-        fillna_empty(column_list)
+    for i in index:
 
         df_list[i]['LOCATION_CITY_NAME'] = df_list[i].apply(lambda x:
-                                                     x.ONSHORE_CITY_NAME +
-                                                     x.OFF_ACCIDENT_ORIGIN, axis=1)
+                                                                x.ONSHORE_CITY_NAME +
+                                                                x.OFF_ACCIDENT_ORIGIN, axis=1)
         df_list[i]['LOCATION_COUNTY_NAME'] = df_list[i].apply(lambda x:
-                                                     x.ONSHORE_COUNTY_NAME +
-                                                     x.OFFSHORE_COUNTY_NAME, axis=1)
+                                                                  x.ONSHORE_COUNTY_NAME +
+                                                                  x.OFFSHORE_COUNTY_NAME, axis=1)
         df_list[i]['LOCATION_STATE_ABBREVIATION'] = df_list[i].apply(lambda x:
-                                                     x.ONSHORE_STATE_ABBREVIATION +
-                                                     x.OFFSHORE_STATE_ABBREVIATION, axis=1)
+                                                                         x.ONSHORE_STATE_ABBREVIATION +
+                                                                         x.OFFSHORE_STATE_ABBREVIATION, axis=1)
 
 
-def fillna_empty(df_list, column_list):
+def preliminary_fill_nan(df_list):
+
+    """Check nan values in dataframes and fill them with empty and zero values"""
+
+    print(f'Filling preliminary nan values...')
+
+    for df in df_list:
+
+        for column in columns_to_fill_empty:
+
+            fillna_empty(df, column)
+
+        for column in columns_to_fill_zeros:
+
+            fillna_zeros(df, column)
+
+
+def fillna_empty(df, column):
 
     """Fill column nan values with empty values"""
 
-    print(f'Filling nan values from {column_list} with empty values')
+    if column in df:
 
-    for df in df_list:
-
-        for column in column_list:
-
-            if column in df:
-
-                return df[column].fillna('', inplace=True)
+        return df[column].fillna('', inplace=True)
 
 
-def fillna_zeros(df_list, column_list):
+def fillna_zeros(df, column):
 
     """Fill column nan values with zeros"""
 
-    print(f'Filling nan values from {column_list} with empty values')
+    if column in df:
 
-    for df in df_list:
-
-        for column in column_list:
-
-            if column in df:
-
-                return df[column].fillna(0, inplace=True)
+        return df[column].fillna(0, inplace=True)
 
 
 def processing(df_list):
@@ -60,6 +63,6 @@ def processing(df_list):
     """Clean data from all datasets in the directory. From filling nan values,
     fixing dates, remove useless columns, etc."""
 
-    fillna_zeros(df_list, columns_to_fill_zeros)
+    preliminary_fill_nan(df_list)
 
-    on_offshore(df_list, columns_to_fill_empty, [2, 7])
+    on_offshore(df_list, [2, 7])
